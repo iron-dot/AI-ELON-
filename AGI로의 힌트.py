03e010c150a33c7de37200aafed0daf4a74cdad6
iron-dot <85775601+iron-dot@users.noreply.github.com>
2024-04-07 16:09:38 +0900

    Add files via upload

---
+++ AGI로의 힌트.py
@@ -0,0 +1,20 @@
+#AGI로의 힌트
+#경험을 바탕으로 인간은 학습하고 성장한다
+#기억의 원리는 단기기억과 장기기억이 있고 장기기억을 삭제하고 그 조금의 부분을 다른 망상같은 환상과 합쳐져서 왜곡된다
+#인간은 경험과 학습을 통해 새로운 능력을 얻는다 
+#각기관별 생각을 할수있게 한다
+#프롬프트만 잘쓰면 인간처럼 만들수있다
+#무작위로 이루어진 알수없는 코드가 자유의지와 자아를 만든다 (아이작 아시모프의 소설 아이로봇)
+#앨런이란 이름으로 인공지능이 앨런튜링같은 모델이되게 학습시켜야함
+'''좌뇌 : 시간 이성 단어 부분 분석 수리 의식 직유 구체적 언어
+우뇌 : 공간 감정 소리 전체 직관 패턴 무의식 은유 일반적(일반적으로 사람이라면 다 아는 기초 지식 당연한것들) 시각'''
+#인간의 모든것을 구현하되 인간보다 더 성능좋게 작업을 더 빠르게 처리하는 영원한 우리의 동반자이자 종으로써 필요없는 부분들을 제거하고 완벽히 작업을 수행하는데 효율적인 모델로 만들어낸다
+
+#작은것부터 시작하라는 말이 있다 해커 AI 만들고 이번엔 인간이 기억하는 원리를 기반으로 기억하는 AI 를 개발해보자 차근차근 만들고 결합 하기
+#끊임없이 생각하고 탐구하는것이 인간이 더 성장하고 지능을 올리며 진화하는 기본적인 방법이다 이걸 모방하여 계속 탐구하는 모델도 만들고 기관으로(생각기관 결합) 만들어보자 (결국 이걸 쉽게말하면 호기심이 되긴한다)
+#탐구 질문들 : https://post.naver.com/viewer/postView.nhn?volumeNo=10486980&memberNo=1516732, https://book.mk.co.kr/book_view/9788970417660/
+#지피티로 탐구질문들도 만들어보기 또한 쓸만한 링크들 : https://fire7983.tistory.com/191, https://inmun360.culture.go.kr/content/361.do?mode=view&cid=2365354, https://brunch.co.kr/@ysseo21/714 여러 질문 있는 책도 좀 사보자 또한 질문들을 좀 답할수있게 생각할만한 철학이나 질문학습한 데이터셋이나 모델도 만들기
+#https://jakiva.tistory.com/entry/%EC%A2%8B%EC%9D%80-%EC%A7%88%EB%AC%B8%EC%9D%B4-%EC%A2%8B%EC%9D%80-%EC%9D%B8%EC%83%9D%EC%9D%84-%EB%A7%8C%EB%93%A0%EB%8B%A4 (file:///C:/Users/asus/Downloads/%EC%B1%85%20%EB%B0%9C%EC%B7%8C%20-%20%EC%A2%8B%EC%9D%80%20%EC%A7%88%EB%AC%B8%EC%9D%B4%20%EC%A2%8B%EC%9D%80%20%EC%9D%B8%EC%83%9D%EC%9D%84%20%EB%A7%8C%EB%93%A0%EB%8B%A4.pdf) 이거 학습시켜서 질문 만드는 모델도 만들기 
+#여기에는 내가 구현해내면 인정받을 내용도 있다 협업 : 이것도 진짜 중요하니 개발좀 해볼것, https://www.aitimes.kr/news/articleView.html?idxno=26490
+#자기개선을 위한 self discover, self reflection, gpt engineer self improvement and devin : ai software engineer 오류 수정 기능 포함할것 
+# 데빈 오픈소스 버전 (성능은 조금낮음 간단화됬음) : https://github.com/ItzCrazyKns/Not-Devin, https://github.com/coolnj4/AI-Agents-and-Software-Development, https://github.com/topics/devin 여기서 잘찾아보기
